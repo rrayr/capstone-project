9fa17a159dbd7c0c8bd97d2617ab168cbc75cdd8
R3 <businessacademics04@gmail.com>
2026-06-22 05:17:25 -0400

    backend patch

--- financial-thoughts/backend/db.js
+++ financial-thoughts/backend/db.js
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
+
+module.exports = db;
