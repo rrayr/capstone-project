601c83293ee1d868f5fc9b5ec1c8bae7aac65407
R3 <businessacademics04@gmail.com>
2026-06-22 03:50:47 -0400

    backend update

--- financial-thoughts/backend/server.js
+++ financial-thoughts/backend/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-app.listen(3001, () => {
-  console.log(
-    "Server running on port 3001"
-  );
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
